adf488e2d11504585547d44f17db518264847834
Jonathan F. Felipe <jonathanfelipe7346@gmail.com>
2025-07-03 15:12:11 -0300

    Add files via upload

---
+++ frmTelaInicial.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class frmTelaInicial
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lblPremio = new System.Windows.Forms.Label();
+            this.btnIniciar = new System.Windows.Forms.Button();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtPremio = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lblPremio
+            // 
+            this.lblPremio.AutoSize = true;
+            this.lblPremio.Location = new System.Drawing.Point(9, 9);
+            this.lblPremio.Name = "lblPremio";
+            this.lblPremio.Size = new System.Drawing.Size(44, 16);
+            this.lblPremio.TabIndex = 1;
+            this.lblPremio.Text = "nome ";
+            // 
+            // btnIniciar
+            // 
+            this.btnIniciar.Location = new System.Drawing.Point(10, 142);
+            this.btnIniciar.Name = "btnIniciar";
+            this.btnIniciar.Size = new System.Drawing.Size(124, 23);
+            this.btnIniciar.TabIndex = 2;
+            this.btnIniciar.Text = "Iniciar Bingo";
+            this.btnIniciar.UseVisualStyleBackColor = true;
+            this.btnIniciar.Click += new System.EventHandler(this.btnIniciar_Click);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 28);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(494, 22);
+            this.textBox1.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 71);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 16);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Premio";
+            // 
+            // txtPremio
+            // 
+            this.txtPremio.Location = new System.Drawing.Point(12, 99);
+            this.txtPremio.Name = "txtPremio";
+            this.txtPremio.Size = new System.Drawing.Size(494, 22);
+            this.txtPremio.TabIndex = 8;
+            // 
+            // frmTelaInicial
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(543, 292);
+            this.Controls.Add(this.txtPremio);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnIniciar);
+            this.Controls.Add(this.lblPremio);
+            this.Name = "frmTelaInicial";
+            this.Text = "Form2";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label lblPremio;
+        private System.Windows.Forms.Button btnIniciar;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtPremio;
+    }
+}
---
+++ frmTelaInicial.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class frmTelaInicial: Form
+    {
+       
+
+        public frmTelaInicial()
+        {
+            InitializeComponent();
+        }
+
+        string SoNumero(string Texto)
+        {
+            string resultado = "";
+            for (int i = 0; i < Texto.Length; i++)
+            {
+                if (char.IsDigit(Texto[i]))
+                {
+                    resultado += Texto[i];
+                }
+            }
+
+            //Retorno o resultado
+            return resultado;
+        }
+        // verificação se arqurvo existe 
+        bool ArquivoExiste(string caminho)
+        {
+            //verifica se o arquivo existe, o nome do arquivo sera o CPF, 
+            //portanto se já existir, o usuário já possui cadastro
+            return File.Exists(caminho);
+        }
+
+        void GravarArquivo(string caminho, string conteudo)
+        {
+            //grava o conteudo no arquivo
+            //se o arquivo não existir, ele será criado
+            string pasta = Path.GetDirectoryName(caminho);
+
+            if (!Directory.Exists(pasta))
+            {
+                Directory.CreateDirectory(pasta);
+            }
+            File.WriteAllText(caminho, conteudo);
+        }
+
+        string GetDirArquivo(string nomePasta, string nomeArquivo)
+        {
+            //O arquivo será montado: Diretório Raiz do Executavel
+            // nome da pasta: será o tipo de cadastro
+            // nome do arquivo: será o CPF
+
+            string raizExe = AppDomain.CurrentDomain.BaseDirectory;
+            return Path.Combine(raizExe, nomePasta, nomeArquivo + ".txt");
+        }
+
+        public string inicioBingo = DateTime.Now.ToShortTimeString();
+        string GetCadastro()
+        {
+
+            //Iremos concatenar os dados do cadastro para gerear o conteudo do arquivo
+            string cadastro = "Nome do Prêmio: " + txtPremio.Text + Environment.NewLine +
+                            "Horário de início: " + inicioBingo;
+
+            return cadastro;
+        }
+
+        public string caminhoArquivoAtual;
+
+        // Método para salvar o cadastro
+        public void Salvar()
+        {
+            string nome = DateTime.Now.ToShortDateString("");
+            // coloca a data e hora atual em duas strings 
+            string date = DateTime.Now.ToShortDateString();
+            string time = DateTime.Now.ToShortTimeString();
+
+            //cria o caminho completo do arquivo,             NOME DO ARQUIVO: Bingo_01_01_2023_12_00.txt
+            string caminhoCompleto = GetDirArquivo("Bingo", ("Bingo_" + SoNumero(date) + "_" + SoNumero(time)));
+
+            //verifica se o arquivo já existe
+            if (ArquivoExiste(caminhoCompleto))
+            {
+                //se o arquivo já existe, o usuário já possui cadastro
+                MessageBox.Show("Usuário já cadastrado", "Atenção", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+            try
+            {
+                //gravar o arquivo
+                GravarArquivo(caminhoCompleto, GetCadastro());
+
+                //se o arquivo foi gravado com sucesso, exibe uma mensagem de sucesso
+                MessageBox.Show("Registro salvo com sucesso!" + Environment.NewLine + Environment.NewLine +
+                    "Salvo em :" + caminhoCompleto, "Informção", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erro ao salvar o cadastro." + Environment.NewLine + "Erro original: " + ex.Message,
+                    "ERRO", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            }
+            caminhoArquivoAtual = caminhoCompleto;
+
+        }
+        void abrir()
+
+        {
+         
+            Form1 tela = new Form1();
+            tela.ShowDialog();
+        }
+        private void btnIniciar_Click(object sender, EventArgs e)
+        {
+            Salvar();
+
+            abrir();
+        }
+
+
+    }
+}
